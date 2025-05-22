4f9a392e5db72cf36899b44d8a4671a4b36ec13a
Kilykaa <strimotkilera@gmail.com>
2025-05-23 00:22:27 +0300

    ADD: 1 practical complete

---
+++ main.py
@@ -0,0 +1,29 @@
+
+CURRENT_YEAR = 2025
+
+class Daniil:
+    def __init__(self, name=None, surname=None, year=None):
+        self.name = name
+        self.surname = surname
+
+        self.year = year
+
+    def getCourse(self):
+        if self.year is None:
+            return None
+
+        age = CURRENT_YEAR - self.year
+
+        if age < 18:
+            return 0
+        elif 18 <= age < 22:
+            return age - 17
+        else:
+            return 4
+
+    def getList(self):
+        return [self.name, self.surname]
+
+student = Daniil(name='Daniil', surname='Chvyr', year=2007,)
+print(student.getList())
+print(student.getCourse())
